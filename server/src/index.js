02b2375aa66493038d8177840254e48f87749fc4
Adrika Pandey <adrikapandey@Adrikas-MacBook-Air.local>
2026-04-15 00:26:34 +0530

    Connect database before handling requests

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,6 @@
 import app from "./app.js";
 import { connectDatabase } from "./config/db.js";
 
-let connectionPromise;
-
-async function ensureDatabaseConnection() {
-  if (!connectionPromise) {
-    connectionPromise = connectDatabase();
-  }
-
-  await connectionPromise;
-}
-
-app.use(async (_req, _res, next) => {
-  try {
-    await ensureDatabaseConnection();
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+await connectDatabase();
 
 export default app;
